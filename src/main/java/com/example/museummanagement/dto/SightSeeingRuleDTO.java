a245d83a5e4ef42aa57e26f648ff9b9acd2e82c5
blackneko184 <khoinq.184@gmail.com>
2022-09-20 14:27:07 +0700

    commit

---
+++ src/main/java/com/example/museummanagement/dto/SightSeeingRuleDTO.java
@@ -0,0 +1,14 @@
+package com.example.museummanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class SightSeeingRuleDTO {
+    private Integer type;
+
+    private  String name;
+
+    private  String title;
+
+    private String content;
+}
---
+++ src/main/java/com/example/museummanagement/service/impl/SightSeeingRuleServiceImpl.java
@@ -0,0 +1,80 @@
+package com.example.museummanagement.service.impl;
+
+import com.example.museummanagement.dto.SightSeeingRuleDTO;
+import com.example.museummanagement.entity.SightSeeingRule;
+import com.example.museummanagement.repository.SightSeeingRuleRepository;
+import com.example.museummanagement.service.SightSeeingRuleService;
+import com.example.museummanagement.ulti.Constants;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SightSeeingRuleServiceImpl implements SightSeeingRuleService {
+
+    @Autowired
+    private SightSeeingRuleRepository sightSeeingRuleRepository;
+
+    @SneakyThrows
+    @Override
+    public SightSeeingRuleDTO createSightSeeingRule(SightSeeingRuleDTO sightSeeingRuleDTO) {
+        Optional<SightSeeingRule> optionalSightSeeingRule = sightSeeingRuleRepository.findByName(sightSeeingRuleDTO.getName());
+        SightSeeingRule sightSeeingRule = new SightSeeingRule();
+        if (optionalSightSeeingRule.isEmpty()) {
+            sightSeeingRule.setName(sightSeeingRuleDTO.getName());
+            sightSeeingRule.setTitle(sightSeeingRuleDTO.getTitle());
+            sightSeeingRule.setContent(sightSeeingRuleDTO.getContent());
+            sightSeeingRule.setType(Constants.TYPE_SIGHTSEEING_RULES);
+            sightSeeingRule.setStatus(Constants.STATUS_ACTIVE);
+            sightSeeingRuleRepository.save(sightSeeingRule);
+        } else {
+            throw new Exception("Name existed!");
+        }
+        return sightSeeingRuleDTO;
+    }
+
+    @SneakyThrows
+    @Override
+    public SightSeeingRuleDTO updateSightSeeingRule(SightSeeingRuleDTO sightSeeingRuleDTO, Long id) {
+        Optional<SightSeeingRule> optionalSightSeeingRule = sightSeeingRuleRepository.findById(id);
+        if (optionalSightSeeingRule.isPresent()) {
+            SightSeeingRule sightSeeingRule = optionalSightSeeingRule.get();
+            Optional<SightSeeingRule> optional = sightSeeingRuleRepository.findByName(sightSeeingRuleDTO.getName());
+            if(optional.isEmpty() || sightSeeingRule.getId().equals(optional.get().getId())) {
+                sightSeeingRule.setName(sightSeeingRuleDTO.getName());
+                sightSeeingRule.setTitle(sightSeeingRuleDTO.getTitle());
+                sightSeeingRule.setContent(sightSeeingRuleDTO.getContent());
+                sightSeeingRule.setType(Constants.TYPE_SIGHTSEEING_RULES);
+                sightSeeingRule.setStatus(Constants.STATUS_ACTIVE);
+                sightSeeingRuleRepository.save(sightSeeingRule);
+            } else {
+                throw new Exception("Name existed!");
+            }
+        } else {
+            throw new Exception("Can not found by id: " + id);
+        }
+        return sightSeeingRuleDTO;
+    }
+
+    @SneakyThrows
+    @Override
+    public void deleteSightSeeingRule(Long id) {
+        List<SightSeeingRule> sightSeeingRuleList = sightSeeingRuleRepository.findAllByIdAndStatus(id,Constants.STATUS_ACTIVE);
+        if (CollectionUtils.isEmpty(sightSeeingRuleList)) {
+            throw new Exception("Can not found!");
+        }
+        for (SightSeeingRule sightSeeingRule: sightSeeingRuleList) {
+            sightSeeingRule.setStatus(Constants.STATUS_INACTIVE);
+            sightSeeingRuleRepository.save(sightSeeingRule);
+        }
+    }
+
+    @Override
+    public List<SightSeeingRule> getAllSightSeeingRule() {
+        return sightSeeingRuleRepository.findAll();
+    }
+}
