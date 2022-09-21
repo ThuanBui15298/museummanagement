845afa8138e5b742cc3663fa3be0855e5988dccb
Admin <123456>
2022-09-21 08:54:33 +0700

    api ins

---
+++ src/main/java/com/example/museummanagement/entity/ProfessionalFunction.java
@@ -0,0 +1,25 @@
+package com.example.museummanagement.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "synthetic")
+public class ProfessionalFunction extends BaseTimeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Integer type;
+
+    private  String name;
+
+    private  String title;
+
+    private String content;
+
+    private Long categoryId;
+
+}
