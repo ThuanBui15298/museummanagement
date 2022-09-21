68611bc1b62b1d0b46e3ca597d139f9e1df5b80f
Admin <123456>
2022-09-21 15:48:41 +0700

    api ins

--- src/main/java/com/example/museummanagement/service/impl/FeaturedNewsServiceImpl.java
+++ src/main/java/com/example/museummanagement/service/impl/FeaturedNewsServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.hibernate.validator.internal.engine.messageinterpolation.parser.MessageDescriptorFormatException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
