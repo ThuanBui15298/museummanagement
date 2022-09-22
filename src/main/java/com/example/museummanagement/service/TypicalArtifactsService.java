ed8dc2f98e796e4e1be53755ccce064dc91f7267
Admin <123456>
2022-09-22 17:05:16 +0700

    api tk

--- src/main/java/com/example/museummanagement/service/TypicalArtifactsService.java
+++ src/main/java/com/example/museummanagement/service/TypicalArtifactsService.java
@@ -1,15 +1,12 @@
 package com.example.museummanagement.service;
 
-import com.example.museummanagement.dto.FeaturedNewsDTO;
 import com.example.museummanagement.dto.TypicalArtifactsDTO;
-import com.example.museummanagement.entity.FeaturedNews;
 import com.example.museummanagement.entity.TypicalArtifacts;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Service
 public interface TypicalArtifactsService {
